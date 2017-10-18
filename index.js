db44342f665e6591b54dd26ce110943b6020237d
Dan Caragea <dancaragea@gmail.com>
2017-10-18 19:52:24 +0300

    update call signature

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
-module.exports = async function serverlessWrapper(lambdaName, forceLocal, fn, args) {
+module.exports = async function serverlessWrapper(lambdaName, forceLocal, obj, fn, args) {
   if (process.env.NODE_ENV !== 'production' || forceLocal) {
-    return fn(...args);
+    return obj[fn](...args);
   }
 
   const lambda = new AWS.Lambda({ region: process.env.AWS_REGION || 'eu-west-1', apiVersion: '2015-03-31' });
@@ -12,8 +12,11 @@ module.exports = async function serverlessWrapper(lambdaName, forceLocal, fn, ar
       FunctionName: lambdaName,
       InvocationType: 'RequestResponse',
       LogType: 'None',
+      // LogType: 'Tail',
+      Payload: JSON.stringify({ fn, args }),
     }).promise();
 
+    // console.warn('data.log', Buffer.from(data.LogResult, 'base64').toString('utf8'));
     return JSON.parse(data.Payload);
   } catch (err) {
     return Promise.reject(err);
